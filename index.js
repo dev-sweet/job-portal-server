13f336323d1acf8b8392065709b5a3dce3cb5202
Sweet Ali <sweetali0520@gmail.com>
2023-12-31 02:56:49 +0600

    basic setup and somthing

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/error");
+
+// call dotenv
+require("dotenv").config();
+
+// port
+const port = process.env.PORT || 8000;
+
+// database connection
+mongoose
+  .connect(process.env.URI)
+  .then(() => console.log("Database connection success!"))
+  .catch((err) => console.log(err));
+
+// app
+const app = express();
+
+// middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "5mb",
+    extended: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(cors);
+
+// error handler middleware
+app.use(errorHandler);
+// listen the app
+app.listen(port, () => {
+  console.log(`Server is running at port: ${port}`);
+});
